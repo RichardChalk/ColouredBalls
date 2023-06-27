19288873341c2f70ada598da7639d524d1c6e667
Richard Chalk <barmybrit@gmail.com>
2023-06-27 19:05:51 +0200

    Add project files.

---
+++ ColouredBalls/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColouredBalls
+{
+    public class Ball
+    {
+        private Colour colour;
+        private float size;
+        private int timesThrown;
+
+        public Ball(Colour colour, float size)
+        {
+            this.colour = colour;
+            this.size = size;
+            this.timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            size = 0;
+        }
+
+        public void Throw()
+        {
+            if (size > 0)
+            {
+                timesThrown++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return timesThrown;
+        }
+    }
+}
---
+++ ColouredBalls/Colour.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColouredBalls
+{
+    public class Colour
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+
+        public Colour(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        public Colour(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 255;
+        }
+
+        public byte GetRed()
+        {
+            return red;
+        }
+
+        public void SetRed(byte red)
+        {
+            this.red = red;
+        }
+
+        public byte GetGreen()
+        {
+            return green;
+        }
+
+        public void SetGreen(byte green)
+        {
+            this.green = green;
+        }
+
+        public byte GetBlue()
+        {
+            return blue;
+        }
+
+        public void SetBlue(byte blue)
+        {
+            this.blue = blue;
+        }
+
+        public byte GetAlpha()
+        {
+            return alpha;
+        }
+
+        public void SetAlpha(byte alpha)
+        {
+            this.alpha = alpha;
+        }
+
+        public byte GetGrayscale()
+        {
+            return (byte)((red + green + blue) / 3);
+        }
+    }
+}
